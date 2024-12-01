6c56689ce24028d85ad067bdbddc028644ceb2b4
Quang Ngo <46818455+quangngonz@users.noreply.github.com>
2024-12-01 22:52:54 +0700

    Update .gitignore and refactor engine.js for improved logging and readability

--- engine.js
+++ engine.js
@@ -1,33 +1,27 @@
-const { database } = require('./services/firebaseService');
-const { ref, get } = require('firebase/database');
+const {database} = require('./services/firebaseService');
+const {ref, get} = require('firebase/database');
 
 // Create a loop that runs every 1000 milliseconds
 const engine = async function () {
-  //log current time
+  // Log current time
   console.log('Current time:', new Date().toISOString());
+  console.log('I am an engine running the ISPH Stock Exchange');
 
   const eventRef = ref(database, `events`);
   const snapshot = await get(eventRef);
+
   const events = snapshot.val();
 
-  const exampleEvent = events[Object.keys(events)[0]];
-  const eventHeader = Object.keys(exampleEvent).join(', ');
-
-  // Colour code the output for eventHeader
-  console.log('Keys:', eventHeader);
-
-  console.log('____________________');
+  console.log();
   for (const eventId in events) {
     const event = events[eventId];
     console.log('Event:', event.event_id);
-    console.log(event.event_name);
-    console.log(event.event_description);
+    console.log("\t", event.event_name);
+    console.log("\t", event.event_description);
+    console.log("\t", event.evaluation.system == "" ? "No evaluation" : event.evaluation.system);
   }
+
   console.log('____________________');
-
-  // Log keys
-
-  console.log('I am an engine running the ISPH Stock Exchange');
 };
 
 exports.engine = engine;
