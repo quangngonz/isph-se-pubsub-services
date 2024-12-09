4898739b5d2194d015516aecee26e704839a07ce
Quang Ngo <46818455+quangngonz@users.noreply.github.com>
2024-12-09 20:24:30 +0700

    qol improvement

--- engine.js
+++ engine.js
@@ -10,32 +10,34 @@ const engine = async function (time_passed) {
 
   const eventWeights = await getEventWeights(true);
   const stocks = await getStocks(true);
-  decayStocks(stocks, {tableName: "Stocks after decay"});
+  decayStocks(stocks, {tableName: "Stocks after decay", log: true});
 
   console.log('____________________');
 };
 
-const decayStocks = (stocks, {decayRate = 0.002, tableName = ""} = {}) => {
+const decayStocks = (stocks, {decayRate = 0.002, tableName = "", log = false} = {}) => {
   const decayStockPrice = (stockPrice, decayRate) => {
     return Math.round(stockPrice * (1 - decayRate) * 100) / 100;
   };
-  
+
   if (tableName) {
-    console.log(tableName + ":");
+    console.log(`\n${tableName}:`);
   }
 
-  const table = new Table({
-    head: Object.keys(stocks[Object.keys(stocks)[0]]),
-    wordWrap: true,
-  });
+  if (log) {
+    const table = new Table({
+      head: Object.keys(stocks[Object.keys(stocks)[0]]),
+      wordWrap: true,
+    });
 
-  for (const stockKey in stocks) {
-    const stock = stocks[stockKey];
-    stock['current_price'] = decayStockPrice(stock['current_price'], decayRate);
-    table.push(Object.values(stock));
+    for (const stockKey in stocks) {
+      const stock = stocks[stockKey];
+      stock['current_price'] = decayStockPrice(stock['current_price'], decayRate);
+      table.push(Object.values(stock));
+    }
+
+    console.log(table.toString());
   }
-
-  console.log(table.toString());
 }
 
 
